9ef66648e2072beaf5219a8b368d755dca7d209e
LaurenNightingale <73908447+LaurenNightingale@users.noreply.github.com>
2021-02-06 11:38:28 +0100

    Versuch sidebar

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource'
+      },
     ],
   },
   plugins: [
